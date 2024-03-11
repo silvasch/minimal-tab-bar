334e4d6b0434540adf1d65b54bd01091265655c2
Silvan Schmidt <mail@silvanschmidt.ch>
2024-03-11 13:40:37 +0100

    feat: display current pane name

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,18 @@ use zellij_tile::prelude::*;
 #[derive(Default)]
 struct Plugin {
     tabs: Vec<TabInfo>,
+    pane_manifest: PaneManifest,
     mode: InputMode,
 }
 
 impl ZellijPlugin for Plugin {
     fn load(&mut self, _: BTreeMap<String, String>) {
         request_permission(&[PermissionType::ReadApplicationState]);
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate]);
+        subscribe(&[
+            EventType::ModeUpdate,
+            EventType::TabUpdate,
+            EventType::PaneUpdate,
+        ]);
     }
 
     fn update(&mut self, event: Event) -> bool {
@@ -27,6 +32,10 @@ impl ZellijPlugin for Plugin {
                 self.tabs = tabs;
                 should_render = true;
             }
+            Event::PaneUpdate(pane_manifest) => {
+                self.pane_manifest = pane_manifest;
+                should_render = true;
+            }
             _ => {}
         }
 
@@ -39,9 +48,11 @@ impl ZellijPlugin for Plugin {
         }
 
         let mut tab_strings: Vec<ANSIString> = vec![];
+        let mut tab_index = None;
 
         for tab in &self.tabs {
             let style = if tab.active {
+                tab_index = Some(tab.position);
                 Style::new().bold().on(Color::Green)
             } else {
                 Style::new()
@@ -49,24 +60,32 @@ impl ZellijPlugin for Plugin {
 
             tab_strings.push(style.paint(format!(" {} ", tab.name)));
         }
-
         let tab_string = ANSIStrings(&tab_strings);
+
+        let active_pane_name = if let Some(tab_index) = tab_index {
+            format!(
+                " {}",
+                match zellij_tile::shim::get_focused_pane(tab_index, &self.pane_manifest) {
+                    Some(pane) => pane.title.clone(),
+                    None => String::new(),
+                }
+            )
+        } else {
+            String::new()
+        };
 
         let mode = format!("{:?} ", self.mode).to_uppercase();
 
-        let mode_section_length = mode.len();
-        dbg!(&mode_section_length);
         let tabs_section_length = unstyled_len(&tab_string);
-        dbg!(&tabs_section_length);
-
-        dbg!(&cols);
+        let pane_name_section_length = active_pane_name.len();
+        let mode_section_length = mode.len();
 
         let mut spacer = String::new();
-        for _ in 0..cols - tabs_section_length - mode_section_length {
+        for _ in 0..cols - tabs_section_length - pane_name_section_length - mode_section_length {
             spacer.push(' ');
         }
 
-        print!("{}{}{}", tab_string, spacer, mode);
+        print!("{}{}{}{}", tab_string, active_pane_name, spacer, mode);
     }
 }
 
